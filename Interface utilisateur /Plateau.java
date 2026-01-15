f541711d12c567a133cfd2e05713460846febb85
mhbe-tech <mbdiece@gmail.com>
2026-01-15 17:13:00 +0100

    Create Plateau.java
    Création de la grille du quoridor

---
+++ Interface utilisateur /Plateau.java
@@ -0,0 +1,57 @@
+package com.quoridor;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+
+public class Plateau extends Pane {
+    
+    // Constantes de dimensions 
+    public static final int TAILLE_CASE = 40;
+    public static final int ESPACE_MUR = 10;
+    public static final int NB_CASES = 9;
+
+    // Tableau pour stocker les cases (utile pour le Jour 3 ou 4 quand on cliquera dessus)
+    private Rectangle[][] grilleCases;
+
+    public Plateau() {
+        this.grilleCases = new Rectangle[NB_CASES][NB_CASES];
+        dessinerGrille();
+    }
+
+    private void dessinerGrille() {
+        // 1. Calcul de la taille totale du plateau
+        int tailleTotale = (NB_CASES * TAILLE_CASE) + ((NB_CASES - 1) * ESPACE_MUR);
+        this.setPrefSize(tailleTotale, tailleTotale);
+        
+        
+        this.setStyle("-fx-background-color: #4a3b2a;"); 
+
+        // 2. Création de la grille 9x9
+        for (int i = 0; i < NB_CASES; i++) {       
+            for (int j = 0; j < NB_CASES; j++) {   
+                
+                // Formule de décalage : Index * (Taille de la case + Épaisseur du mur)
+                int x = j * (TAILLE_CASE + ESPACE_MUR);
+                int y = i * (TAILLE_CASE + ESPACE_MUR);
+
+                Rectangle caseJeu = new Rectangle(x, y, TAILLE_CASE, TAILLE_CASE);
+                caseJeu.setFill(Color.BURLYWOOD); 
+                caseJeu.setStroke(Color.BLACK);
+                
+              
+                caseJeu.setOnMouseEntered(e -> caseJeu.setFill(Color.rgb(240, 210, 160)));
+                caseJeu.setOnMouseExited(e -> caseJeu.setFill(Color.BURLYWOOD));
+
+                
+                grilleCases[i][j] = caseJeu;
+                this.getChildren().add(caseJeu);
+            }
+        }
+    }
+
+    public Rectangle getCase(int ligne, int colonne) {
+        return grilleCases[ligne][colonne];
+    }
+}
