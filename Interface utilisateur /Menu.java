07cb4adc6e508e5afa9e75211d3c7999207ead83
Zyademz <zyade72@gmail.com>
2026-01-23 14:08:00 +0100

    Update Menu.java
    Modification du programme main pour le connecter aux autres classes

--- Interface utilisateur /Menu.java
+++ Interface utilisateur /Menu.java
@@ -3,31 +3,27 @@ package com.quoridor;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Menu extends Application {
 
-  
     public void start(Stage stage) {
         
-        // 1. Création du Plateau
+        Moteur monMoteur = new Moteur();
         Plateau monPlateau = new Plateau();
         
-        // Joueur 1 (Blanc) en bas au milieu (Ligne 8, Colonne 4)
-        monPlateau.placerPion(8, 4, Color.WHITE);
-        
-        // Joueur 2 (Noir) en haut au milieu (Ligne 0, Colonne 4)
-        monPlateau.placerPion(0, 4, Color.BLACK);
 
-        // 2. Mise en page
+        ControleurJeu controleur = new ControleurJeu(monPlateau, monMoteur);
+        
+        //  Initialisation visuelle basée sur le modèle
+        monPlateau.initialiserPionBlancVisuel(monMoteur.getPionBlancLigne(), monMoteur.getPionBlancCol());
+
+        //  Affichage standard
         StackPane root = new StackPane(monPlateau);
         root.setStyle("-fx-padding: 20; -fx-background-color: #222222;");
 
-        // 3. Création de la scène et configuration de la fenêtre
         Scene scene = new Scene(root);
-        
-        stage.setTitle("Quoridor ");
+        stage.setTitle("Quoridor - Phase MVC (Refactoring)");
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
