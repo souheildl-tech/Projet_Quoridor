0ad946bd6595b069c18f473b6ba6c11d8322bb0b
koukouibo-hub <koukoui.bo@gmail.com>
2026-02-07 14:56:00 +0100

    Update Moteur.java

--- Interface utilisateur /Moteur.java
+++ Interface utilisateur /Moteur.java
@@ -2,15 +2,27 @@ package com.quoridor;
 
 public class Moteur {
 
+    private int pionBlancLigne = 8;
+    private int pionBlancCol = 4;
 
     public Moteur() {
 
     }
 
-    public boolean estDeplacementValide(int departLigne, int departCol, int arriveeLigne, int arriveeCol) {
-        // Calcul de la différence 
-        int diffLigne = Math.abs(departLigne - arriveeLigne);
-        int diffCol = Math.abs(departCol - arriveeCol);
+    // Vérification des règles mathématiques
+    public boolean estDeplacementValide(int arriveeLigne, int arriveeCol) {
+        int diffLigne = Math.abs(this.pionBlancLigne - arriveeLigne);
+        int diffCol = Math.abs(this.pionBlancCol - arriveeCol);
         return (diffLigne + diffCol) == 1;
     }
+
+    // Mise à jour des positions
+    public void majPositionBlanc(int ligne, int col) {
+        this.pionBlancLigne = ligne;
+        this.pionBlancCol = col;
+    }
+
+    // Getters pour que le contrôleur puisse lire les positions
+    public int getPionBlancLigne() { return pionBlancLigne; }
+    public int getPionBlancCol() { return pionBlancCol; }
 }
