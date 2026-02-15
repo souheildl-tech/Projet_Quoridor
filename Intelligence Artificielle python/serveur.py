a9f8983d8f827df09c3ae3630560edb4dc2c62e4
Zyademz <zyade72@gmail.com>
2026-02-15 16:08:00 +0100

    Update serveur.py
    le serveur Python est maintenant capable de décoder les différents types d'actions envoyés par le client Java

--- Intelligence Artificielle python/serveur.py
+++ Intelligence Artificielle python/serveur.py
@@ -4,7 +4,6 @@ import time
 HOST = '127.0.0.1'
 PORT = 65432
 
-# Position de départ de l'IA (Pion Noir)
 ia_ligne = 0
 ia_col = 4
 
@@ -13,41 +12,50 @@ def start_server():
     print(f"--- CERVEAU PYTHON DÉMARRÉ sur {HOST}:{PORT} ---")
     
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((HOST, PORT))
         s.listen()
         
-        print("En attente du corps Java (ControleurJeu)...")
+        print("En attente de l'interface Java...")
         
         conn, addr = s.accept()
         with conn:
             print(f"Connecté au jeu ! ({addr})")
             
             while True:
-                # 1. Attendre le coup du Joueur 
                 data = conn.recv(1024)
                 if not data:
                     print("Déconnexion du client Java.")
                     break
                 
                 msg_recu = data.decode('utf-8').strip()
-                print(f"Le joueur a joué : {msg_recu}")
+                print(f"\n[Reçu] Action du Joueur : {msg_recu}")
                 
-                # 2. Réfléchir (IA très bête : on avance)
-                time.sleep(0.5)
-                
-                
-                ia_ligne += 1
-                
-                # Si l'IA arrive au bout (victoire), elle s'arrête
-                if ia_ligne > 8:
-                    ia_ligne = 8
+            
+                try:
+                    if msg_recu.startswith("MOVE:"):
+                        _, pos = msg_recu.split(":")
+                        l, c = map(int, pos.split(","))
+                        print(f"-> Le joueur Blanc s'est déplacé en [{l}, {c}]")
+                        
+                    elif msg_recu.startswith("WALL:"):
+                        _, params = msg_recu.split(":")
+                        l, c, orient = params.split(",")
+                        print(f"-> Le joueur Blanc a posé un mur en [{l}, {c}] (Orientation: {orient})")
+                        
+                except ValueError as e:
+                    print(f"[ERREUR] Message mal formaté reçu du Java : {msg_recu} -> {e}")
 
-                # 3. Formater et Envoyer la réponse
-                reponse = f"MOVE:{ia_ligne},{ia_col}\n"
-                print(f"L'IA joue : {reponse.strip()}")
+                time.sleep(0.2)
                 
+                # L'IA avance toujours bêtement vers le bas
+                ia_ligne += 1
+                if ia_ligne > 8: 
+                    ia_ligne = 8 
+                
+
+                reponse = f"MOVE:{ia_ligne},{ia_col}\n"
+                print(f"[Envoi] L'IA joue : {reponse.strip()}")
                 conn.sendall(reponse.encode('utf-8'))
 
 if __name__ == "__main__":
